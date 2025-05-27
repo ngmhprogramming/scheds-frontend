cbdc9f47490067d883d35586913a79be198cbde9
ngmh <ngmhcartoon@gmail.com>
2025-05-27 22:21:34 +0800

    Production dockerfiles

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3003,
-    host: true,
-    watch: {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    server: {
+      port: 3003,
+      host: true,
+      watch: {
        usePolling: true,
-    },
-  },
-})
+      },
+      esbuild: {
+       target: "esnext",
+       platform: "linux",
+     },
+   },
+   define: {
+     VITE_APP_BACKEND_ADDRESS: JSON.stringify(env.VITE_APP_BACKEND_ADDRESS),
+   },
+  };
+ });
