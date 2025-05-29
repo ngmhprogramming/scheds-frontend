704a0e89c8a7b711fdcbf86e04a7ee47ab941e30
ngmh <ngmhcartoon@gmail.com>
2025-05-29 22:14:08 +0800

    Add footer

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Container from './components/Container';
+import Footer from './components/Footer';
 
 const Root = () => {
 	return (
 		<>
-			<Navbar />
-			<Container>
-				<h1 className="text-3xl">Root</h1>
-				<div className="card bg-base-100 w-96 shadow-sm">
-					<figure>
-						<img
-							src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-							alt="Shoes" />
-					</figure>
-					<div className="card-body">
-						<h2 className="card-title">Card Title</h2>
-						<p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-						<div className="card-actions justify-end">
-							<button className="btn btn-primary">Buy Now</button>
-						</div>
-					</div>
-				</div>
-			</Container>
+			<div className="flex flex-col min-h-screen">
+				<Navbar />
+				<main className="flex-grow px-4 py-6 max-w-6xl mx-auto">
+					<h1 className="text-3xl">Root</h1>
+				</main>
+				<Footer />
+			</div>
 		</>
 	);
 }
