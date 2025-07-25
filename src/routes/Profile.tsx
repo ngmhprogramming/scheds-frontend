55e7e5e23585b67cbff8ecfb6c297436f89fbd7c
IvorKhan <navil.nanhua@gmail.com>
2025-07-26 01:25:20 +0800

    Reorganised profile page to distinguish login details updates and profile updates.

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -3,7 +3,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import API from './api';
-import { getLocal, setLocal } from "./storage";
+import { getLocal, setLocal, removeLocal } from "./storage";
 
 interface ProfileData {
 	user_id: string,
@@ -24,38 +24,68 @@ const Profile = () => {
 		setProfileData(getLocal("profileData"));
 	}, []);
 
-	const [form, setForm] = useState({
+	const [loginForm, setLoginForm] = useState({
+		email: "",
+		password: "",
+	});
+	const [profileForm, setProfileForm] = useState({
 		username: "",
 		full_name: "",
 		pfp_url: "",
-		email: "",
-		password: "",
 		bio: "",
 	});
 	const [success, setSuccess] = useState<string | null>(null);
-	const [error, setError] = useState("");
-	const [loading, setLoading] = useState(false);
+	const [profileError, setProfileError] = useState("");
+	const [loginError, setLoginError] = useState("");
 
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-		setForm({ ...form, [event.target.name]: event.target.value, });
+	const handleLoginChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		setLoginForm({ ...loginForm, [event.target.name]: event.target.value, });
+	};
+	const handleProfileChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		setProfileForm({ ...profileForm, [event.target.name]: event.target.value, });
 	};
 
-	const handleSubmit = async (event: React.FormEvent) => {
+	const handleLoginSubmit = async (event: React.FormEvent) => {
 		event.preventDefault();
-		setError("");
+		setLoginError("");
 
-		const res = await API.profileUpdate(form);
+		const res = await API.loginUpdate(loginForm);
+		console.log("submitting");
 		if ("error" in res) {
-			setError(res.error);
+			setLoginError(res.error);
 		} else {
 			const updatedProfile = await API.getProfile();
 			if ("error" in updatedProfile) {
-				setError(updatedProfile.error);
+				setLoginError(updatedProfile.error);
 			} else {
+				console.log("updating profileData");
+				removeLocal("profileData");
 				setLocal("profileData", updatedProfile.data);
 			}
 			setProfileData(getLocal("profileData"));
-			navigate("/profile", { state: { success: "Successful event creation!" } });
+			navigate("/profile", { state: { success: "Successful login update!" } });
+		}
+	};
+
+	const handleProfileSubmit = async (event: React.FormEvent) => {
+		event.preventDefault();
+		setProfileError("");
+
+		const res = await API.profileUpdate(profileForm);
+		console.log("submitting");
+		if ("error" in res) {
+			setProfileError(res.error);
+		} else {
+			const updatedProfile = await API.getProfile();
+			if ("error" in updatedProfile) {
+				setProfileError(updatedProfile.error);
+			} else {
+				console.log("updating profileData");
+				removeLocal("profileData");
+				setLocal("profileData", updatedProfile.data);
+			}
+			setProfileData(getLocal("profileData"));
+			navigate("/profile", { state: { success: "Successful profile update!" } });
 		}
 	};
 
@@ -78,109 +108,132 @@ const Profile = () => {
 					<div className="bg-base-100 shadow-md rounded-lg p-6 mb-8">
 						<div className="min-h-[500px] flex">
 							{profileData && (
-								<form onSubmit={handleSubmit} className="bg-base-100 shadow-md rounded-lg p-6">
-									<fieldset className="space-y-4">
-										<div className="w-30 pr-4 rounded-full">
-											{/* TODO: Provide fallback image if there is an error */}
-											<img
-												alt="User avatar"
-												src={profileData.pfp_url}
-											/>
-										</div>
-
-										<div className="form-control max-w-sm">
-											<label htmlFor="username" className="label">
-												<span className="label-text">Username</span>
-											</label>
-											<input
-												id="username"
-												name="username"
-												type="text"
-												placeholder={profileData.username}
-												className="input input-bordered w-full"
-												value={form.username}
-												onChange={handleChange}
-											/>
-										</div>
-
-										<div className="form-control max-w-sm">
-											<label htmlFor="full_name" className="label">
-												<span className="label-text">Full Name</span>
-											</label>
-											<input
-												id="full_name"
-												name="full_name"
-												type="text"
-												placeholder={profileData.full_name}
-												className="input input-bordered w-full"
-												value={form.full_name}
-												onChange={handleChange}
-											/>
-										</div>
-
-										<div className="form-control max-w-sm">
-											<label htmlFor="email" className="label">
-												<span className="label-text">Email</span>
-											</label>
-											<input
-												id="email"
-												name="email"
-												type="email"
-												className="input input-bordered w-full"
-												value={form.email}
-												onChange={handleChange}
-											/>
-										</div>
-
-										<div className="form-control max-w-sm">
-											<label htmlFor="password" className="label">
-												<span className="label-text">Password</span>
-											</label>
-											<input
-												id="password"
-												name="password"
-												type="password"
-												className="input input-bordered w-full"
-												value={form.password}
-												onChange={handleChange}
-											/>
-										</div>
-
-										<div className="form-control max-w-lg">
-											<label htmlFor="bio" className="label">
-												<span className="label-text">Bio</span>
-											</label>
-											<textarea
-												id="bio"
-												name="bio"
-												className="textarea textarea-bordered w-full"
-												placeholder={profileData.bio}
-												value={form.bio}
-												onChange={handleChange}
-											/>
-										</div>
-
-										<button type="submit" className="btn btn-primary">Update Profile</button>
-
-										{error && (
-											<div role="alert" className="alert alert-error">
-												<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-													<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-												</svg>
-												<span>{error}</span>
+								<>
+									<form onSubmit={handleProfileSubmit} className="bg-base-100 shadow-md rounded-lg p-6 pr-20">
+										<fieldset className="space-y-4">
+											<h1 className="text-xl font-bold mb-6">Update Profile</h1>
+											<div className="w-30 pr-4 rounded-full hover:opacity-20">
+												{/* TODO: Provide fallback image if there is an error */}
+												<img
+													alt="User avatar"
+													src={profileData.pfp_url}
+												/>
 											</div>
-										)}
-
-										{error && (
-											<div role="alert" className="alert alert-error">
-												<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-													<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-												</svg>
-												<span>{error}</span>
+											<div className="form-control max-w-sm">
+												<label htmlFor="pfp_url" className="label">
+													<span className="label-text">New Profile Picture URL</span>
+												</label>
+												<input
+													id="pfp_url"
+													name="pfp_url"
+													type="text"
+													placeholder={profileData.pfp_url}
+													className="input input-bordered w-full"
+													value={profileForm.pfp_url}
+													onChange={handleProfileChange}
+												/>
 											</div>
-										)}
-									</fieldset>
-								</form>
+
+											<div className="form-control max-w-sm">
+												<label htmlFor="username" className="label">
+													<span className="label-text">Username</span>
+												</label>
+												<input
+													id="username"
+													name="username"
+													type="text"
+													placeholder={profileData.username}
+													className="input input-bordered w-full"
+													value={profileForm.username}
+													onChange={handleProfileChange}
+												/>
+											</div>
+
+											<div className="form-control max-w-sm">
+												<label htmlFor="full_name" className="label">
+													<span className="label-text">Full Name</span>
+												</label>
+												<input
+													id="full_name"
+													name="full_name"
+													type="text"
+													placeholder={profileData.full_name}
+													className="input input-bordered w-full"
+													value={profileForm.full_name}
+													onChange={handleProfileChange}
+												/>
+											</div>
+
+											<div className="form-control max-w-lg">
+												<label htmlFor="bio" className="label">
+													<span className="label-text">Bio</span>
+												</label>
+												<textarea
+													id="bio"
+													name="bio"
+													className="textarea textarea-bordered w-full"
+													placeholder={profileData.bio}
+													value={profileForm.bio}
+													onChange={handleProfileChange}
+												/>
+											</div>
+
+											<button type="submit" className="btn btn-primary">Update Profile</button>
+
+											{profileError && (
+												<div role="alert" className="alert alert-error">
+													<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+														<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+													</svg>
+													<span>{profileError}</span>
+												</div>
+											)}
+										</fieldset>
+									</form>
+									<form onSubmit={handleLoginSubmit} className="bg-base-100 shadow-md rounded-lg p-6">
+										<fieldset className="space-y-4">
+											<h1 className="text-xl font-bold mb-6">Update Login Details</h1>
+											<div className="form-control max-w-sm">
+												<label htmlFor="email" className="label">
+													<span className="label-text">Email</span>
+												</label>
+												<input
+													id="email"
+													name="email"
+													type="email"
+													className="input input-bordered w-full"
+													value={loginForm.email}
+													onChange={handleLoginChange}
+												/>
+											</div>
+
+											<div className="form-control max-w-sm">
+												<label htmlFor="password" className="label">
+													<span className="label-text">Password</span>
+												</label>
+												<input
+													id="password"
+													name="password"
+													type="password"
+													className="input input-bordered w-full"
+													value={loginForm.password}
+													onChange={handleLoginChange}
+												/>
+											</div>
+
+											<button type="submit" className="btn btn-primary">Update Login Details</button>
+
+											{loginError && (
+												<div role="alert" className="alert alert-error">
+													<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+														<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+													</svg>
+													<span>{loginError}</span>
+												</div>
+											)}
+										</fieldset>
+									</form>
+								</>
 							)}
 						</div>
 					</div>
--- src/routes/api/profile.ts
+++ src/routes/api/profile.ts
@@ -1,17 +1,36 @@
 import axios from "axios";
 
 export interface newProfileData {
-		username: string,
-		full_name: string,
-		pfp_url: string,
-		email: string,
-		password: string,
-		bio: string,
+	username: string,
+	full_name: string,
+	pfp_url: string,
+	bio: string,
 };
+
+export interface newLoginData {
+	email: string,
+	password: string,
+}
 
 export const profileUpdate = async (data: newProfileData) => {
 	const backendAddress = import.meta.env.VITE_APP_BACKEND_ADDRESS;
-	const res = await axios.post(backendAddress + "profile/update", data,
+	const res = await axios.post(backendAddress + "profile/update/profile", data,
+		{
+			headers: { 'Content-Type': 'multipart/form-data' },
+			withCredentials: true
+		})
+		.then(res => {
+			return res.data;
+		})
+		.catch(() => {
+			return { error: "API is down!"}
+		});
+	return res;
+};
+
+export const loginUpdate = async (data: newLoginData) => {
+	const backendAddress = import.meta.env.VITE_APP_BACKEND_ADDRESS;
+	const res = await axios.post(backendAddress + "profile/update/login", data,
 		{
 			headers: { 'Content-Type': 'multipart/form-data' },
 			withCredentials: true
